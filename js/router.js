78d4f2297ce3cab5cd9f6780ceaa096b35067d7f
Harold <advinculaharoldl.official@gmail.com>
2024-12-27 16:02:30 +0800

    create css/ js/ view/

---
+++ js/router.js
@@ -0,0 +1,9 @@
+
+    const getNav = document.getElementsByTagName("nav")[0].addEventListener("click", ()=> {console.log("im click"); return 0;});
+    console.log(getNav)
+
+
+
+const href = document.querySelector('[href]').getAttribute('href');
+// history.pushState()
+

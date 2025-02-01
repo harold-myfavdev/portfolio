614e8e192561445452453717829bfff4e1242db1
harold <haroldadvincula@myfav.dev>
2025-02-01 23:00:36 +0800

    change email; add copyright to html2pdf

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ function generatePDF() {
     const btn = document.querySelector('button');
     btn.setAttribute('data-html2canvas-ignore','true');
 
-    const copyright = document.querySelector('footer p');
-    copyright.setAttribute('data-html2canvas-ignore','true');
+    // const copyright = document.querySelector('footer p');
+    // copyright.setAttribute('data-html2canvas-ignore','true');
 
     // const svgs = document.querySelectorAll('section#projects svg');    
     // svgs.forEach(svg => {
@@ -24,19 +24,30 @@ function generatePDF() {
     body.style.overflow = 'hidden';
     body.style.width = '600px';
 
+    const contactInfo = document.querySelector('#content > footer > ul');
+    const getStyleDisplay = contactInfo.style.display;
+    const getStylePaddingTop = contactInfo.style.paddingTop;
+    // const getStylePaddingBottom = contactInfo.style.paddingBottom;
+    contactInfo.style.display = 'block';
+    contactInfo.style.paddingTop = '0';
+    // contactInfo.style.paddingBottom = '0';
+    
+
     /* Set Options */
     var opt = {        
         margin:       0,
         pagebreak: { mode: 'avoid-all'},        
-        filename:     'myfile.pdf',
-        image:        { type: 'png'},
+        filename:     'harold.webdev.pdf',
+        image:        { type: 'jpeg', quality: 1},
+        enableLinks: true,
         html2canvas:  { 
             scale: 1, 
             foreignObjectRendering: true,             
             scrollY: 0,
             y: 0,     
             width: 600,
-            height: 1504,                
+            height: 1504,     
+            logging: true,          
         },
         jsPDF: { 
             unit: 'px',             
@@ -52,6 +63,9 @@ function generatePDF() {
         body.style.backgroundColor = '#00000081';
         body.style.overflow = 'scroll';
         body.style.width = '100%';
+        contactInfo.style.display = getStyleDisplay;
+        contactInfo.style.paddingTop = getStylePaddingTop;
+        // contactInfo.style.paddingBottom = getStylePaddingBottom;
         // svgs.forEach(svg => {
         //     svg.style.display = "inline";        
         // })    

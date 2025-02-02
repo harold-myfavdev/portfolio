ff74c24ab13a975f5ac3f6fd247aa475c91af278
harold <haroldadvincula@myfav.dev>
2025-02-02 12:55:09 +0800

    clean code

--- main.js
+++ main.js
@@ -1,38 +1,25 @@
 /* HTML2PDF Generator */
 function generatePDF(scales) {
-     /* Tag elements that will be ignored*/
+    /* Tag elements that will be ignored*/
     const btns = document.querySelectorAll('button');
     btns.forEach(btn => {btn.setAttribute('data-html2canvas-ignore','true');});
 
-    // const copyright = document.querySelector('footer p');
-    // copyright.setAttribute('data-html2canvas-ignore','true');
-
-    // const svgs = document.querySelectorAll('section#projects svg');    
-    // svgs.forEach(svg => {
-    //     svg.setAttribute('data-html2canvas-ignore','true');
-    // }); 
-
-    /* Temporary Remove Elements before generating pdf */
-    // svgs.forEach(svg => {
-    //     svg.style.display = "none";        
-    // })    
-
     /* Temporary set properties before generating pdf */
+    /* Modify Body */
     const body = document.body;
     body.style.setProperty("--before-text", null);
     body.style.backgroundColor = 'white';
     body.style.overflow = 'hidden';
     body.style.width = '600px';
+    console.log(body.style.height);
 
+    /* Modify Footer Contact Details */
     const contactInfo = document.querySelector('#content > footer > ul');
     const getStyleDisplay = contactInfo.style.display;
     const getStylePaddingTop = contactInfo.style.paddingTop;
-    // const getStylePaddingBottom = contactInfo.style.paddingBottom;
     contactInfo.style.display = 'block';
     contactInfo.style.paddingTop = '0';
-    // contactInfo.style.paddingBottom = '0';
     
-
     /* Set Options */
     const width = 600; /* Preferred 600 */
     const height  = 1504; /* Preferred 1504 */
@@ -41,14 +28,14 @@ function generatePDF(scales) {
     var marginRight = compStyle.getPropertyValue('margin-right');
     const documentWidth = document.documentElement.clientWidth;
     var x;
+
+    /* Magic Numbers of HTML2PDF v0.10.2 Scale Option */
     if (parseFloat(documentWidth) <= 600 ){ x = 0;};
     switch (scale) {      
       case 1: if (x != 0) x = parseFloat(marginRight)*.50; console.log(scale); break;
       case 2: if (x != 0) x = parseFloat(marginRight)*.3355; console.log(scale); break;
       case 3: if (x != 0) x = parseFloat(marginRight)*.25; console.log(scale); break;
-    }
-
-    console.log(marginRight);
+    }    
 
     var opt = {        
         margin:       0,
@@ -84,17 +71,13 @@ function generatePDF(scales) {
         body.style.width = '100%';
         contactInfo.style.display = getStyleDisplay;
         contactInfo.style.paddingTop = getStylePaddingTop;
-        // contactInfo.style.paddingBottom = getStylePaddingBottom;
-        // svgs.forEach(svg => {
-        //     svg.style.display = "inline";        
-        // })    
     }
     convert2pdf();    
   } 
 /* End HTML2PDF Generator */
 
 /* Function for Current Year */
-const currentYear = new Date().getFullYear();
-const getYearEl = document.querySelector('.year');
-getYearEl.innerHTML = currentYear;
+  const currentYear = new Date().getFullYear();
+  const getYearEl = document.querySelector('.year');
+  getYearEl.innerHTML = currentYear;
 /* End Function for Current Year */
